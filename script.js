0d3bde0384ba33d5173ac2bc97938f048ea9d046
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-19 19:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
-
+const form = document.querySelector('#form1');
+form.addEventListener('submit' , getFormvalue);
+function getFormvalue(e)
+{
+    e.preventDefault();
+    const formData = new FormData(this);
+    alert(`${formData.get('fname')} ${formData.get('lname')}`);
 }
